3abc4e546608758e3f627739d243c682d7cc597c
alyoshaswamy <raghav.ravishankar11@gmail.com>
2024-02-22 19:56:22 +0530

    kdks

--- bunny/model/__init__.py
+++ bunny/model/__init__.py
@@ -1,5 +1,4 @@
 from .language_model.bunny_phi import BunnyPhiForCausalLM, BunnyPhiConfig
 from .language_model.bunny_stablelm import BunnyStableLMForCausalLM, \
 	BunnyStableLMConfig
-from .language_model.bunny_qwen import BunnyQwenForCausalLM, BunnyQwenConfig
 from .language_model.bunny_gemma import BunnyGemmaForCausalLM, BunnyGemmaConfig
